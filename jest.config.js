d55935b60190f856cc5c49917a1d020049f119bd
Marco Link <marco.link@contentful.com>
2025-12-08 16:49:18 +0100

    fix: remove node types dependency

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.test.json' }]
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setup.tests.js']
 };
